d470edf3dd694812b6af770f7344f5f1764ef04d
David Antônio Gonçalves <pet0104207@aluno.firjansenaisesi.com.br>
2026-04-27 21:24:03 -0300

    adicionando um decorator (ainda não implementado)

---
+++ src/decorators/isBrPhone.ts
@@ -0,0 +1,14 @@
+import { ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
+
+@ValidatorConstraint ({name: "isBrPhone", async: false})
+export class IsBrPhoneContraint implements ValidatorConstraintInterface{
+    validate(value: unknown): boolean {
+        if (typeof value !== "string") return false;
+
+        const phoneRegex = /^\(?\d{2}\)?\s?9\d{4}-?d{4}$/;
+        return phoneRegex.test(value);        
+    }
+    defaultMessage(args?: ValidationArguments): string {
+        return `O numero ${args?.value} não é válido.`;
+    }
+}
